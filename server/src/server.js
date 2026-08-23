0643dd0195ee415b19fb4b9bc0f4b955737ad816
UtkarshRode <utkarshrode2809@gmail.com>
2026-08-23 18:20:36 +0530

    Fix production CORS configuration

--- server/src/server.js
+++ server/src/server.js
@@ -39,6 +39,7 @@ const CLIENT_URL =
 app.use(
     cors({
         origin: [
+            CLIENT_URL,
             "http://localhost:5173",
             /^http:\/\/192\.168\.\d+\.\d+:5173$/
         ]
@@ -58,10 +59,11 @@ const io = new Server(
     server,
     {
         cors: {
-            origin: [
-                "http://localhost:5173",
-                /^http:\/\/192\.168\.\d+\.\d+:5173$/
-            ],
+    origin: [
+        CLIENT_URL,
+        "http://localhost:5173",
+        /^http:\/\/192\.168\.\d+\.\d+:5173$/
+    ],
             methods: [
                 "GET",
                 "POST"
