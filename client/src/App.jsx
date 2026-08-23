e7aacbc1bc6d62e79df069db3b12898bf92b3f95
UtkarshRode <utkarshrode2809@gmail.com>
2026-08-23 18:53:10 +0530

    Fix authentication routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,11 @@ import {
 
 import Dashboard from "./pages/Dashboard";
 import LiveLearningRoom from "./pages/LiveLearningRoom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Course from "./pages/Course";
+import Lesson from "./pages/Lesson";
+import Assessment from "./pages/Assessment";
 
 
 function App() {
@@ -21,20 +26,41 @@ function App() {
                     path="/"
                     element={
                         <Navigate
-                            to="/dashboard"
+                            to="/login"
                             replace
                         />
                     }
                 />
 
+                <Route
+                    path="/login"
+                    element={<Login />}
+                />
+
+                <Route
+                    path="/register"
+                    element={<Register />}
+                />
 
                 <Route
                     path="/dashboard"
-                    element={
-                        <Dashboard />
-                    }
+                    element={<Dashboard />}
                 />
 
+                <Route
+                    path="/course/:courseId"
+                    element={<Course />}
+                />
+
+                <Route
+                    path="/lesson/:lessonId"
+                    element={<Lesson />}
+                />
+
+                <Route
+                    path="/assessment/:assessmentId"
+                    element={<Assessment />}
+                />
 
                 <Route
                     path="/live-learning/:roomId"
@@ -43,12 +69,11 @@ function App() {
                     }
                 />
 
-
                 <Route
                     path="*"
                     element={
                         <Navigate
-                            to="/dashboard"
+                            to="/login"
                             replace
                         />
                     }
